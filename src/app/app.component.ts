66ce136b5adca55826d2790b8a16d027b97b012b
Andrej Chub <mega.chub@mail.ru>
2016-11-02 16:48:12 +0200

    dynamic table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,24 @@ export class AppComponent {
   title: string = 'Welcome to YouTube API!';
   public name: string;
   public results:Array<any>=[];
+  public movies:Array<any>=[];
 
-  constructor() {
+  constructor() {    
+    this.movies = [{
+      title:'JavaScript',
+      url:'(link)',
+      channel:'aaa',
+      views:20000
+    },
+    {
+      title:'ES6',
+      url:'(link)',
+      channel:'ppp',
+      views:350603
+    }
+    ];
     this.name = 'YouTube';
-    this.results[0]={url:'wdaw',title:'wdfg'};
-    this.results[1]={url:'zxcf',title:'qqq'};
+    this.results[0]={url:'(link)',title:'Java Script'};
+    this.results[1]={url:'(link)',title:'C++'};
   }
 }
